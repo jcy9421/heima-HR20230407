3147822261a7210edf5c2399fc74940d8efbf4d4
J-Chunyu <jichunyu@gbkj.onaliyun.com>
2023-04-12 17:15:35 +0800

    完成登录表单结构

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,13 +5,27 @@
       <h1>登录</h1>
       <el-card shadow="never" class="login-card">
         <!--登录表单-->
+        <el-form>
+          <el-form-item>
+            <el-input placeholder="请输入手机号"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input placeholder="请输入密码"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-checkbox>用户平台使用协议</el-checkbox>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" style="width: 350px">登录</el-button>
+          </el-form-item>
+        </el-form>
       </el-card>
     </div>
   </div>
 </template>
 <script>
 export default {
-  name: "Login"
+  name: 'Login'
 }
 </script>
 <style lang="scss">
