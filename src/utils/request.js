cf00e41ec14dbd6c66a1ad1b2c7ef7dde097179b
J-Chunyu <jichunyu@gbkj.onaliyun.com>
2023-04-18 16:30:15 +0800

    token 失效处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import store from '@/store'
 import Message from 'element-ui/packages/message'
+import router from '@/router'
 
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API,
@@ -21,19 +22,21 @@ service.interceptors.request.use(
   }
 )
 // 响应拦截
-service.interceptors.response.use(
-  (response) => {
-    const { data, message, success } = response.data
-    if (success) {
-      return data
-    } else {
-      Message({ type: 'error', message })
-      return Promise.reject(new Error(message))
-    }
-  },
-  (error) => {
-    Message({ type: 'error', message: 'error.message' })
+service.interceptors.response.use((response) => {
+  const { data, message, success } = response.data
+  if (success) {
+    return data
+  } else {
+    Message({ type: 'error', message })
+    return Promise.reject(new Error(message))
+  }
+},
+async(error) => {
+  if (error.response.status === 401) {
+    Message({ type: 'warning', message: 'token超时了' })
+    await store.dispatch('user/loginOut')
+    router.push('/login')
     return Promise.reject(error)
   }
-)
+})
 export default service
