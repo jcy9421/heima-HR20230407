64a76e7eb384c103a4442b35ebdfddfe029e80fb
纪纯瑜 <jcy9421@hotmail.com>
2023-04-24 10:26:58 +0800

    角色编辑,进入编辑状态

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -11,15 +11,18 @@
       </div>
       <!--      表格-->
       <el-table :data="roleList">
-        <el-table-column
-          prop="name"
-          align="center"
-          width="300px"
-          label="角色"
-        />
+        <el-table-column prop="name" align="center" width="300px" label="角色">
+          <template v-slot="{ row }">
+            <!--            条件判断-->
+            <el-input v-if="row.isEdit" size="mini" />
+            <span v-else>{{ row.name }}</span>
+          </template>
+        </el-table-column>
         <el-table-column prop="state" align="center" width="300px" label="启用">
           <template v-slot="{ row }">
+            <el-switch v-if="row.isEdit" />
             <el-tag
+              v-else
               :type="getTagColor(row.state)"
             >{{
               row.state === 1 ? "已启用" : row.state === 0 ? "未启用" : "无"
@@ -27,12 +30,29 @@
             </el-tag>
           </template>
         </el-table-column>
-        <el-table-column prop="description" align="center" label="描述" />
+        <el-table-column prop="description" align="center" label="描述">
+          <template v-slot="{ row }">
+            <!--            条件判断-->
+            <el-input v-if="row.isEdit" type="textarea" size="mini" />
+            <span v-else>{{ row.description }}</span>
+          </template>
+        </el-table-column>
         <el-table-column align="center" label="操作">
-          <template>
-            <el-button size="mini" type="text">分配权限</el-button>
-            <el-button size="mini" type="text">编辑</el-button>
-            <el-button size="mini" type="text">删除</el-button>
+          <template v-slot="{ row }">
+            <template v-if="row.isEdit">
+              <el-button size="mini" type="primary">确定</el-button>
+              <el-button size="mini">取消</el-button>
+            </template>
+            <template v-else>
+              <el-button size="mini" type="text">分配权限</el-button>
+              <el-button
+                size="mini"
+                type="text"
+                @click="btnEditRow(row)"
+              >编辑
+              </el-button>
+              <el-button size="mini" type="text">删除</el-button>
+            </template>
           </template>
         </el-table-column>
       </el-table>
@@ -80,6 +100,11 @@ export default {
       const { rows, total } = await getRoleList(this.pageParams)
       this.roleList = rows
       this.pageParams.total = total
+      this.roleList.forEach((item) => {
+        // 添加一个属性，初始值为false
+        // item.isEdit = false   动态添加不具备响应式特性
+        this.$set(item, 'isEdit', false) // this.$set(目标对象,属性名,初始值)
+      })
     },
     getTagColor(state) {
       if (state === 1) {
@@ -91,6 +116,9 @@ export default {
     changePage(newPage) {
       this.pageParams.page = newPage
       this.getRoleList()
+    },
+    btnEditRow(row) {
+      row.isEdit = true
     }
   }
 }
