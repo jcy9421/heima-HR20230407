f8617910dda60f9f283beb1d5a83672dd44a738a
纪纯瑜 <jcy9421@hotmail.com>
2023-04-28 16:50:18 +0800

    员工详情结构及路由配置

--- src/router/modules/employee.js
+++ src/router/modules/employee.js
@@ -1,14 +1,25 @@
 import layout from '@/layout'
+
 export default {
   path: '/employee',
   component: layout,
-  children: [{
-    path: '',
-    name: 'employee',
-    component: () => import('@/views/employee'),
-    meta: {
-      title: '员工',
-      icon: 'people'
+  children: [
+    {
+      path: '',
+      name: 'employee',
+      component: () => import('@/views/employee'),
+      meta: {
+        title: '员工',
+        icon: 'people'
+      }
+    },
+    {
+      path: '/employee/detail',
+      component: () => import('@/views/employee/detail.vue'),
+      hidden: true,
+      meta: {
+        title: '员工详情'
+      }
     }
-  }]
+  ]
 }
