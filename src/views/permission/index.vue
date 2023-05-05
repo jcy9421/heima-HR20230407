5b201d758b6e2e7142c67c3da48c96ef4ce5044c
纪纯瑜 <jcy9421@hotmail.com>
2023-05-05 14:48:35 +0800

    权限表格渲染并展示树形结构

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -1,12 +1,48 @@
 <template>
   <div class="container">
     <div class="app-container">
-      <el-button type="primary">添加权限</el-button>
+      <el-button type="primary" class="btn-add" size="mini">添加权限</el-button>
+      <el-table :data="permissionList" row-key="id" :default-expand-all="true">
+        <el-table-column label="名称" prop="name" />
+        <el-table-column label="标识" prop="code" />
+        <el-table-column label="描述" prop="description" />
+        <el-table-column label="操作">
+          <template v-slot="{row}">
+            <el-button v-if="row.type===1" type="text" size="mini">添加</el-button>
+            <el-button type="text" size="mini">编辑</el-button>
+            <el-button type="text" size="mini">删除</el-button>
+
+          </template>
+        </el-table-column>
+      </el-table>
     </div>
   </div>
 </template>
 <script>
+import { getPermissionList } from '@/api/permission'
+import { transListToTreeData } from '@/utils'
+
 export default {
-  name: 'Permission'
+  name: 'Permission',
+  data() {
+    return {
+      permissionList: []
+    }
+  },
+  created() {
+    this.getPermissionList()
+  },
+  methods: {
+    async getPermissionList() {
+      const result = transListToTreeData(await getPermissionList(), 0)
+      this.permissionList = result
+      console.log(this.permissionList)
+    }
+  }
 }
 </script>
+<style>
+.btn-add {
+  margin: 10px;
+}
+</style>
